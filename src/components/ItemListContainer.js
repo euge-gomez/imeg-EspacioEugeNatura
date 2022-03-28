25433a1ef7018e2dbc1fc2c55420b4d1aad5675d
eugeGomez <eugequimica@gmail.com>
2022-03-27 22:35:08 -0300

    Aniadido de a

--- src/components/ItemListContainer.js
+++ src/components/ItemListContainer.js
@@ -1,6 +1,6 @@
 const ItemListContainer = () => {
     return(
-        <h3 className="m-3">Bienvenido mi Espacio Natura</h3>
+        <h3 className="m-3">Bienvenido a mi Espacio Natura</h3>
     );
 }
 export default ItemListContainer;
