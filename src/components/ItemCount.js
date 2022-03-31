48331c6220edee96c976764e728f7cbc664a6d4d
eugeGomez <eugequimica@gmail.com>
2022-03-31 17:58:27 -0300

    Desafio ItemCount

---
+++ src/components/ItemCount.js
@@ -0,0 +1,28 @@
+import { useState } from "react";
+
+const ItemCount = ({stock, initial, onAdd}) => {
+
+    const [quantity, setCounter]=useState(1);
+
+    const increment = () => {
+        if (quantity<stock) {
+            setCounter(quantity+1)
+        }
+    }
+
+    const substract = () => {
+        if (quantity>initial){
+            setCounter(quantity-1)
+        }
+    }
+
+    return(
+        <div className="m-3">
+            <button className="btn-secondary m-2" onClick={increment}>+</button>
+            <span>{quantity}</span>
+            <button className="btn-secondary m-2" onClick={substract}>-</button>
+            <button className="btn-primary" onClick={onAdd}>Añadir al Carro</button>
+        </div>
+    );
+}
+export default ItemCount;
--- src/components/ItemListContainer.js
+++ src/components/ItemListContainer.js
@@ -1,8 +1,16 @@
-import { Wrapper } from './styledComponents'
+import ItemCount from './ItemCount'
 
 const ItemListContainer = ({greeting}) => {
+
+    const onAdd = (quantity) => {
+        alert('Has seleccionado ' + quantity + " productos.")
+    }
+
     return(
-        <Wrapper> {greeting} </Wrapper>
+        <div> {greeting} 
+        <br/>
+        <ItemCount stock={4} initial={1} onAdd={onAdd}/>
+        </div>
     );
 }
 export default ItemListContainer;
